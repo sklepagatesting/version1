fc205f8146fc3c9af2fd1751695b9cfdab7b5f9e
sklepagatesting <sklepagainformationportal@gmail.com>
2025-08-02 10:56:44 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,7 @@ document.addEventListener("DOMContentLoaded", () => {
   }, { passive: true });
 
   // --- Touch Swipe Logic ---
-  const touchScrollMultiplier = 0.5; // Increase for more sensitivity
+  const touchScrollMultiplier = 0.3; // Increase for more sensitivity
   let startY;
   let isDraggingDown = false;
 
